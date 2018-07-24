86e22de10f03bce79077497d77601cc2c03188bc
xinerkeke <943164165@qq.com>
2018-07-24 15:54:32 +0800

    commit BaseTest

--- src/main/java/byy/testMaven/BaseTest.java
+++ src/main/java/byy/testMaven/BaseTest.java
@@ -1,15 +1,18 @@
 package main.java.byy.testMaven;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
 	private ParseProperties prop;
 	private ParseProperties xpathProp;
-	protected WebDriver driver = null;
+	protected static WebDriver driver = null;
 	private Do find;
 
 	private static String testdata = "\\src\\main\\java\\byy\\testMaven\\test.properties";
@@ -34,6 +37,46 @@ public class BaseTest {
 		driver.findElement(By.id("loginPassword")).clear();
 		driver.findElement(By.id("loginPassword")).sendKeys("usda");
 		driver.findElement(By.id("loginBtn")).click();
+	}
+	
+	/*
+	 * 遍历body节点下的所有节点 取其文本值 并判断是否包含文本 what
+	 */
+	public static boolean isTextPresent(WebDriver driver, String what) {
+		try {
+			
+			String str = driver.findElement(By.tagName("body")).getText();
+			System.out.println(str);
+			
+			return str.contains(what);
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	/*
+	 * 获取屏幕列表数据中的某行，返回第一个列表元素 tableXpath:列表xpath， 到行（.../tr） serial：在第几个元素中查找
+	 * what：匹配的字符串
+	 */
+	public static WebElement getTableFirstElt(WebDriver driver,
+			String tableXpath, int serial, String what) {
+
+		if (what == null || tableXpath == null)
+			return null;
+
+		List<WebElement> rows = driver.findElements(By.xpath(tableXpath));
+
+		for (WebElement row : rows) {
+			// 得到当前tr里td的集合
+			List<WebElement> cols = row.findElements(By.tagName("td"));
+
+			String str = cols.get(serial).getText();
+			if (cols.get(serial).getText().contains(what)) {
+				//return cols.get(0);
+				return row;
+			}
+		}
+		return null;
 	}
 
 }
